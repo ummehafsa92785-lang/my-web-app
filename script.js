5cf4f339041c2ec2ad9990ef9dcdac57ba9f354a
ummehafsa92785-lang <ummehafsa92785@gmail.com>
2025-09-20 01:38:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -208,3 +208,33 @@ function escapeHtml(text) {
     return ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' })[m];
   });
 }
+
+let map3DElement = null;
+async function init() {
+    const { Map3DElement } = await google.maps.importLibrary("maps3d");
+    { }
+    map3DElement = new Map3DElement({
+        center: { lat: 51.532, lng: -0.124, altitude: 30 },
+        range: 1400,
+        tilt: 64,
+        heading: -5,
+        mode: 'HYBRID'
+    });
+    document.body.append(map3DElement);
+    map3DElement.addEventListener('gmp-click', async (event) => {
+        event.preventDefault();
+        if (event.placeId) {
+            const place = await event.fetchPlace();
+            await place.fetchFields({ fields: ['*'] });
+            // Display place details.
+            document.getElementById("placeName").innerHTML = "<b>Name :</b><br>&nbsp;" + place.displayName;
+            document.getElementById("placeId").innerHTML = "<b>Id :</b><br>&nbsp;" + place.id;
+            document.getElementById("placeType").innerHTML = "<b>Types :<b/>";
+            for (const type of place.types) {
+                document.getElementById("placeType").innerHTML += "<br>&nbsp;" + type;
+            }
+            document.getElementById("details").style.display = "block";
+        }
+    });
+}
+init();

38c3abc05987f8ac373b9824e6519e902d53c348
ummehafsa92785-lang <ummehafsa92785@gmail.com>
2025-09-19 23:19:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFxlAmbWAPUbOToqgDLLg539NOGqsvRSA",
+  apiKey: "AIzaSyCnrlIcS9z8Y09WJ5nr_0yiMdQjkEScSTY",
   authDomain: "bangladeshjobsmap.firebaseapp.com",
   projectId: "bangladeshjobsmap",
   storageBucket: "bangladeshjobsmap.firebasestorage.app",
